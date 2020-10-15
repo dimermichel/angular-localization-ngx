5a2ac059ec97a9f5331c747867986e9755522b66
Michel Maia <dimermichel@gmail.com>
2020-10-15 16:29:26 -0400

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslationService } from './services/translation.service';
+import { locale as en } from '../assets/i18n/en';
+import { locale as de } from '../assets/i18n/de';
+import { locale as fr } from '../assets/i18n/fr';
+import { locale as pt } from '../assets/i18n/pt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-localization';
+export class AppComponent implements OnInit{
+  title = 'angular-localization-ngx';
+  constructor( private translationService: TranslationService) {
+    this.translationService.loadTranslations(en, de, fr, pt);
+  }
+
+  ngOnInit() {
+    this.translationService.setLanguage(this.translationService.getSelectedLanguage());
+  }
 }
